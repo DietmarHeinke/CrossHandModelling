e671c3609378c17907c3f994d44095a17e3e0489
Robert Dragomir <robbertica97@yahoo.com>
2020-09-27 16:38:33 +0100

    code update

--- run_touch.m
+++ run_touch.m
@@ -1,20 +1,26 @@
 %nr_trials = [10, 40, 50, 100, 100, 100, 100, 100, 10, 10, 10, 10, 10, 10];
-nr_trials = 20 + zeros( 1, 40 );
-acc_count = [];
+nr_trials = 10 + zeros( 1, 40);
+acc_count1 = [];
+acc_count2 = [];
+acc_count3 = [];
 w = randn(12,1)/1000;
 prior = [];
 y_tot = [];
+noise_var = 0.7;
 for (n=nr_trials)
     cond = [-1 1];
-    accuracy=0;
+    accuracy=[0 0 0];
+    input = 0;
     for (i=1:n)
         body_space = randi(2);
         if (body_space==1)
             I_L=1;
             I_R=0;
+            input = normrnd(-1,noise_var);
         else
             I_L=0;
             I_R=1;
+            input = normrnd(1,noise_var);
         end
         
         x=rand;
@@ -25,37 +31,79 @@ for (n=nr_trials)
         end
 
         crossed = cond(select);
-        [wo,correct,y_exp, y_pred]=touch(I_L,I_R,crossed,w);
+        [wo,correct,y_exp, y_pred]=touch2(input,I_L,I_R,crossed,w);
         w=wo;
-        accuracy=accuracy+correct;
-        
+        accuracy(1)=accuracy(1)+correct(1);
+        accuracy(2)=accuracy(2)+correct(2);
+        accuracy(3)=accuracy(3)+correct(3);
         condition = [body_space, y_exp];
         prior = cat(1,prior,condition);
        
     end
-    y = [y_pred, proba_touch(body_space,prior), y_exp, crossed];
+    y = [y_pred, proba_touch2(input,prior), y_exp, crossed];
     y_tot = cat(1,y_tot,y);
         
-    acc_count=[acc_count accuracy/n];
+    acc_count1=[acc_count1 accuracy(1)/n];
+    acc_count2=[acc_count2 accuracy(2)/n];
+    acc_count3=[acc_count3 accuracy(3)/n];
 end
 i= 1:length(nr_trials);
 figure
-plot(i, acc_count)
-title('Final accuracy score after training for n iterations')
+plot(i, acc_count1)
+plot_title = title('Final accuracy score after training for n iterations (stage 1)');
+x_label = xlabel('session');
+y_label = ylabel('accuracy');
+xticklabels(i)
+ylim([0 1.2])
+
+figure
+plot(i, acc_count2)
+title('Final accuracy score after training for n iterations (stage 2)')
 xlabel('number of training iterations') 
 ylabel('accuracy')
 xticklabels([10, 50, 100, 200, 300, 400, 500, 600])
 ylim([0 1.2])
 
-ind = y_tot(:,4) == 1;
+figure
+plot(i, acc_count3)
+title('Final accuracy score after training for n iterations (stage 3)')
+xlabel('number of training iterations') 
+ylabel('accuracy')
+xticklabels([10, 50, 100, 200, 300, 400, 500, 600])
+ylim([0 1.2])
+
+ind = y_tot(:,6) == -1;
 y_tot = y_tot(ind,:);
 i= 1:length(y_tot(:,1));
 figure
-plot(i, y_tot(:,1))
+plot(i, y_tot(:,4))
 hold on
-plot(i, y_tot(:,2))
-scatter(i, y_tot(:,3),40,'v','g')
+plot(i, y_tot(:,1))
+scatter(i, y_tot(:,5),40,'v','g')
 hold off
 ylim([-1.2 1.7])
-legend('slow model', 'quick model', 'expected')
+legend('quick model', 'slow model - stg 1', 'expected')
 
+ind = y_tot(:,6) == -1;
+y_tot = y_tot(ind,:);
+i= 1:length(y_tot(:,1));
+figure
+plot(i, y_tot(:,4))
+hold on
+plot(i, y_tot(:,2))
+scatter(i, y_tot(:,5),40,'v','g')
+hold off
+ylim([-1.2 1.7])
+legend('quick model', 'slow model - stg 2', 'expected')
+
+ind = y_tot(:,6) == -1;
+y_tot = y_tot(ind,:);
+i= 1:length(y_tot(:,1));
+figure
+plot(i, y_tot(:,4))
+hold on
+plot(i, y_tot(:,3))
+scatter(i, y_tot(:,5),40,'v','g')
+hold off
+ylim([-1.2 1.7])
+legend('quick model', 'slow model - stg 3', 'expected')
--- touch2.m
+++ touch2.m
@@ -1,4 +1,5 @@
 function [wo,correct,y_exp, y_pred] = touch2(input,I_L,I_R,crossed,w)
+noise_var = 0.7;
 if (crossed==1)
     I_1 = 1;
     I_2 = -1;
@@ -6,56 +7,68 @@ else
     I_1 = -1;
     I_2 = 1;
 end
-alpha = 0.006;
-x1 = I_1*w(1) + I_2*w(2);
-x2 = I_1*w(3) + I_2*w(4);
+y_pred = [];
+correct = [];
+for (i=1:3)
+    
+    alpha = 0.001;
+    x1 = I_1*w(1) + I_2*w(2);
+    x2 = I_1*w(3) + I_2*w(4);
 
-y_L = x1*input*w(5) + x2*input*w(7);
-y_R = x1*input*w(9) + x2*input*w(11);
+    y_L = x1*input*w(5) + x2*input*w(6);
+    y_R = x1*input*w(7) + x2*input*w(8);
 
-y_exp = crossed * (I_L - I_R);
-if (y_exp == -1 && y_L > y_R)
-    w(5) = w(5) + alpha*x1*input;
-    w(7) = w(7) + alpha*x2*input;
-    w(1) = w(1) + alpha*(I_1*input*w(5));
-    w(2) = w(2) + alpha*(I_2*input*w(5));
-    w(3) = w(3) + alpha*(I_1*input*w(7));
-    w(4) = w(4) + alpha*(I_2*input*w(7));
-    correct=1;
-end
-if (y_exp == -1 && y_R > y_L)
-    w(9) = w(9) - alpha*x1*input;
-    w(11) = w(11) - alpha*x2*input;
-    w(1) = w(1) - alpha*(I_1*input*w(9));
-    w(2) = w(2) - alpha*(I_2*input*w(9));
-    w(3) = w(3) - alpha*(I_1*input*w(11));
-    w(4) = w(4) - alpha*(I_2*input*w(11));
-    correct=0;
-end
-if (y_exp == 1 && y_L > y_R)
-    w(5) = w(5) - alpha*x1*input;
-    w(7) = w(7) - alpha*x2*input;
-    w(1) = w(1) - alpha*(I_1*input*w(5));
-    w(2) = w(2) - alpha*(I_2*input*w(5));
-    w(3) = w(3) - alpha*(I_1*input*w(7));
-    w(4) = w(4) - alpha*(I_2*input*w(7));
-    correct=0;
-end
-if (y_exp == 1 && y_R > y_L)
-    w(9) = w(9) + alpha*x1*input;
-    w(11) = w(11) + alpha*x2*input;
-    w(1) = w(1) + alpha*(I_1*input*w(9));
-    w(2) = w(2) + alpha*(I_2*input*w(9));
-    w(3) = w(3) + alpha*(I_1*input*w(11));
-    w(4) = w(4) + alpha*(I_2*input*w(11));
-    correct=1;
+    y_exp = crossed * (I_L - I_R);
+    if (y_exp == -1 && y_L > y_R)
+        w(5) = w(5) + alpha*x1*input;
+        w(6) = w(6) + alpha*x2*input;
+        w(1) = w(1) + alpha*(I_1*input*w(5));
+        w(2) = w(2) + alpha*(I_2*input*w(5));
+        w(3) = w(3) + alpha*(I_1*input*w(6));
+        w(4) = w(4) + alpha*(I_2*input*w(6));
+        correct=[correct 1];
+    end
+    if (y_exp == -1 && y_R > y_L)
+        w(7) = w(7) - alpha*x1*input;
+        w(8) = w(8) - alpha*x2*input;
+        w(1) = w(1) - alpha*(I_1*input*w(7));
+        w(2) = w(2) - alpha*(I_2*input*w(7));
+        w(3) = w(3) - alpha*(I_1*input*w(8));
+        w(4) = w(4) - alpha*(I_2*input*w(8));
+        correct=[correct 0];
+    end
+    if (y_exp == 1 && y_L > y_R)
+        w(5) = w(5) - alpha*x1*input;
+        w(6) = w(6) - alpha*x2*input;
+        w(1) = w(1) - alpha*(I_1*input*w(5));
+        w(2) = w(2) - alpha*(I_2*input*w(5));
+        w(3) = w(3) - alpha*(I_1*input*w(6));
+        w(4) = w(4) - alpha*(I_2*input*w(6));
+        correct=[correct 0];
+    end
+    if (y_exp == 1 && y_R > y_L)
+        w(7) = w(7) + alpha*x1*input;
+        w(8) = w(8) + alpha*x2*input;
+        w(1) = w(1) + alpha*(I_1*input*w(7));
+        w(2) = w(2) + alpha*(I_2*input*w(7));
+        w(3) = w(3) + alpha*(I_1*input*w(8));
+        w(4) = w(4) + alpha*(I_2*input*w(8));
+        correct=[correct 1];
+    end
+    
+    if (y_L > y_R)
+        y_pred = [y_pred -1];
+    else
+        y_pred = [y_pred 1];
+    end
+
+    if (I_L==1)
+        input = (input + normrnd(-1,noise_var))/2;
+    else
+        input = (input + normrnd(1,noise_var))/2;
+    end
 end
 wo=[];
-for (i=1:12)
+for (i=1:8)
     wo = [wo w(i)];
-end
-if (y_L > y_R)
-    y_pred = -1;
-else
-    y_pred = 1;
 end
